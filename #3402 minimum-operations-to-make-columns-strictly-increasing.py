77fac254e8b9e01a9ea16531412002d9ff028275
j-last <148649001+j-last@users.noreply.github.com>
2025-01-29 16:29:51 +0000

    Update #3402 minimum-operations-to-make-columns-strictly-increasing.py

--- #3402 minimum-operations-to-make-columns-strictly-increasing.py
+++ #3402 minimum-operations-to-make-columns-strictly-increasing.py
@@ -1,18 +1,18 @@
+"""A fairly easy one where the most intuitive solution is the best one.
+"""
+
 def minimumOperations(grid: list[list[int]]) -> int:
     operations = 0
     for col in range(len(grid[0])):
+        # Keep track of the number above the current number 
+        # (starting with the first number in the column).
         last_num = grid[0][col]
         for row in range(1, len(grid)):
             num = grid[row][col]
             if num <= last_num:
+                # The number has to become one more than the last number.
                 last_num += 1
                 operations += (last_num - num)
             else:
                 last_num = num
     return operations        
-
-print(
-    minimumOperations(
-        [[3,2],[1,3],[3,4],[0,1]]
-    )
-)
